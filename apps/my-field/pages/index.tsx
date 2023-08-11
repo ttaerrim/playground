666e4de9c6ecad19a5acfff7c7564b8fb37ef172
ttaerrim <ltr0121@naver.com>
2023-08-11 19:07:48 +0900

    modify: index link

--- apps/my-field/pages/index.tsx
+++ apps/my-field/pages/index.tsx
@@ -1,12 +1,41 @@
 import styled from "@emotion/styled";
-import WaterDrop from "../components/WaterDrop";
+import { Zilla_Slab } from "@next/font/google";
+import Link from "next/link";
 
-const StyledPage = styled.div``;
+const zilla = Zilla_Slab({
+  weight: ["700"],
+  style: ["italic"],
+  display: "swap",
+});
+
+const StyledPage = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  gap: 20px;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    font-family: ${zilla.style.fontFamily};
+    font-size: 40px;
+    color: #c1c1c1;
+    transition: color 0.3s, font-size 0.3s;
+
+    &:hover {
+      color: #3892fe;
+      font-size: 45px;
+    }
+  }
+`;
 
 export function Index() {
   return (
     <StyledPage>
-      <WaterDrop />
+      <Link href="water-drop" className="water-drop">
+        Water Drop
+      </Link>
+      <Link href="input">Input</Link>
     </StyledPage>
   );
 }
---
+++ apps/my-field/pages/water-drop/index.tsx
@@ -0,0 +1,14 @@
+import styled from "@emotion/styled";
+import WaterDrop from "../../components/WaterDrop";
+
+const StyledPage = styled.div``;
+
+export function Index() {
+  return (
+    <StyledPage>
+      <WaterDrop />
+    </StyledPage>
+  );
+}
+
+export default Index;
