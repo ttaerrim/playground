9b9585d3d59bbadc44ca79053db4c670c76b416b
ttaerrim <ltr0121@naver.com>
2023-08-22 23:09:14 +0900

    style: background color

--- apps/my-field/pages/web-animation/progress/index.tsx
+++ apps/my-field/pages/web-animation/progress/index.tsx
@@ -1,13 +1,7 @@
 import styled from "@emotion/styled";
-import { Noto_Sans_KR } from "@next/font/google";
 import { gsap } from "gsap";
 import { useEffect, useRef } from "react";
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-
-const noto_sans = Noto_Sans_KR({
-  weight: ["400"],
-  display: "swap",
-});
 
 const StyledPage = styled.div`
   position: relative;
@@ -15,36 +9,57 @@ const StyledPage = styled.div`
   > div {
     width: 100vw;
     height: 200vh;
-    background-color: aliceblue;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+    font-size: 4.5rem;
+
+    background: linear-gradient(to bottom, #ffe0e0, #cfe5ff);
   }
 
   .progress {
     width: 100vw;
-    height: 0.5em;
-    background: red;
-    transform-origin: 0 50%;
-    transform: scaleX(0);
+    height: 0.5rem;
+    background: transparent;
     position: fixed;
     top: 0;
     left: 0;
+
+    &::-webkit-progress-bar {
+      background: transparent;
+    }
+    &::-webkit-progress-value {
+      background: linear-gradient(
+        to right,
+        rgba(255, 0, 0, 1),
+        rgba(255, 160, 0, 1),
+        rgba(230, 255, 0, 1),
+        rgba(75, 255, 0, 1),
+        rgba(0, 255, 88, 1),
+        rgba(0, 255, 229, 1),
+        rgba(0, 84, 255, 1),
+        rgba(121, 0, 255, 1),
+        rgba(255, 0, 235, 1),
+        rgba(255, 0, 131, 1),
+        rgba(255, 0, 0, 1)
+      );
+      background-attachment: fixed;
+    }
   }
 `;
 
 export function Index() {
-  const progressRef = useRef<HTMLDivElement>(null);
+  const progressRef = useRef<HTMLProgressElement>(null);
   gsap.registerPlugin(ScrollTrigger);
 
   useEffect(() => {
-    // const animation = progressRef.current.animate(
-    //   [{ transform: "scaleX(0)" }, { transform: "scaleX(1)" }],
-    //   { duration: 2500, fill: "forwards", easing: "linear" }
-    // );
-
     ScrollTrigger.create({
       trigger: "#section",
       start: "top 0",
       end: "bottom bottom",
-      animation: gsap.fromTo(progressRef.current, { scaleX: 0 }, { scaleX: 1 }),
+      animation: gsap.fromTo(progressRef.current, { value: 0 }, { value: 100 }),
       scrub: true,
     });
   }, []);
@@ -52,7 +67,9 @@ export function Index() {
   return (
     <StyledPage>
       <div id="section">
-        <div className="progress" ref={progressRef} />
+        <progress className="progress" max="100" value="70" ref={progressRef} />
+        <p>Scroll to add some color to your life! 🎨</p>
+        <p>You're doing great, keep going 😘</p>
       </div>
     </StyledPage>
   );
